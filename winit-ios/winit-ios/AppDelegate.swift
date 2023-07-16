37dddb0fc0a089a0f75812648ac637615419065b
U32Float <123883732+U32Float@users.noreply.github.com>
2023-07-16 14:07:16 +0200

    init

---
+++ winit-ios/winit-ios/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  winit-ios
+//
+//  Created by Joep Kneefel on 16/07/2023.
+//
+
+import UIKit
+
+@main
+class MyApp {
+    static func main() {
+        start_app();
+    }
+}
---
+++ winit-ios/winit-ios/Bridge.h
@@ -0,0 +1,12 @@
+//
+//  Bridge.h
+//  winit-ios
+//
+//
+
+#ifndef Bridge_h
+#define Bridge_h
+
+void start_app();
+
+#endif /* Bridge_h */
