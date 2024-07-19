b7d691cc567cde272f9f9d08619c7026c85ac532
alanpoon <brightshadowx@gmail.com>
2024-07-19 22:47:31 +0800

    t

--- crates/app/src/lib.rs
+++ crates/app/src/lib.rs
@@ -58,32 +58,51 @@ pub fn new_main(eventloop:EventLoop<()>,android_app:AndroidApp){
 
 
 }
-pub fn new(eventloop:EventLoop<()>){
-    ambient_git_rev_init::init().expect("Should be called exactly once");
+use glam::{uvec2};
+static mut QUIT:bool = false;
+static mut LOADED:bool = false;
+#[cfg(target_os="ios")]
+pub fn new(){
+    let mut rt = ambient_sys::task::make_native_multithreaded_runtime().unwrap();
 
-    shared::components::init().unwrap();
+    let runtime = rt.handle();
+    let assets: AssetCache = AssetCache::new(runtime.clone());
+    let _settings = SettingsKey.get(&assets);
+    //box_c();
+    let in_size: winit::dpi::PhysicalSize<u32> = self.window.clone().unwrap().inner_size();
+    let width = in_size.width;
+    let height = in_size.height;
+    let headless = Some(uvec2(width, height));
+    let scale_factor = self
+    .window
+    .as_ref()
+    .map(|x| x.scale_factor() as f32)
+    .unwrap_or(1.) as f64;
+    tracing::info!("scale_factor {:?}",scale_factor);
+    rt.block_on(async move {
+        let mut app = AppBuilder::new()
+            .ui_renderer(true)
+            .with_asset_cache(assets)
+            .headless(headless)
+            .update_title_with_fps_stats(false)
+            .build(window).await.unwrap();
 
-    let audio_bool = false;
-    let audio_stream = if audio_bool {
-        match AudioStream::new() {
-            Ok(v) => Some(v),
-            Err(err) => {
-                tracing::error!("Failed to initialize audio stream: {err}");
-                None
-            }
+        *app.world.resource_mut(window_scale_factor()) = scale_factor;
+
+        i_c.call(&mut app,android_app_c).await;
+        *app_.lock() = Some(app);
+        unsafe{
+            LOADED = true;
         }
-    } else {
-        None
-    };
+        //use tokio::time::{sleep, Duration};
+        let quit = unsafe{
+            QUIT
+        };
+        use std::time::{Duration};
+        use std::thread::sleep;
+        loop{
+            sleep(Duration::new(5,0));
 
-    let is_debug = false;
-    //box_init();
-
-    tracing::info!("before event_loop");
-    let  aw = AppWrapper::new_with_event_loop(eventloop);
-
-    tracing::info!("after event_loop");
-    let status = aw.run_blocking(client::init2,Box::new(||{
-        shared::components::init().unwrap();
-    }));
+        }
+    });
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,11 @@ use winit::{event_loop::EventLoopBuilder, window::{Window, WindowBuilder}};
 use ambient;
 #[mobile_entry_point]
 pub extern "C" fn main() {
-    let event_loop = EventLoopBuilder::new().build();
+    //let event_loop = EventLoopBuilder::new().build();
     // let window = WindowBuilder::new().build(&event_loop).unwrap();
     
     // event_loop.run(move |event, _, _| {
     //     println!("{:?}", event);
     // });
-    ambient::new(event_loop);
+    ambient::run();
 }
