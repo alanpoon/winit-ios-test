194ec8d8fe240d9897379236cdabc8e79c1d66af
alanpoon <brightshadowx@gmail.com>
2024-07-22 15:25:02 +0800

    UIApplication

--- winit-ios/winit-ios/AppDelegate.swift
+++ winit-ios/winit-ios/AppDelegate.swift
@@ -5,11 +5,23 @@
 //  Created by Joep Kneefel on 16/07/2023.
 //
 
+// import UIKit
+
+// @main
+// class MyApp {
+//     static func main() {
+//         start_app();
+//     }
+// }
+
 import UIKit
 
-@main
-class MyApp {
-    static func main() {
-        start_app();
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        start_app()
+        return true
     }
 }
