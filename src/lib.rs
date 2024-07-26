2132ed515ebe8f09804b89c07834d8af0e267aab
alanpoon <brightshadowx@gmail.com>
2024-07-26 20:53:54 +0800

    winit30

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,28 @@
 use mobile_entry_point::mobile_entry_point;
-use winit::{event_loop::EventLoopBuilder, window::{Window, WindowBuilder}};
-use ambient;
 #[mobile_entry_point]
 pub extern "C" fn main() {
-    let event_loop = EventLoopBuilder::new().build();
-    // let window = WindowBuilder::new().build(&event_loop).unwrap();
-    
-    // event_loop.run(move |event, _, _| {
-    //     println!("{:?}", event);
-    // });
-    ambient::new(event_loop);
+    use winit::{
+        application::ApplicationHandler,
+        event::WindowEvent,
+        event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
+        window::WindowId,
+    };
+
+    struct App;
+
+    impl ApplicationHandler for App {
+        fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+            event_loop.set_control_flow(ControlFlow::Wait);
+        }
+        fn window_event(&mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent) {}
+        fn suspended(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+            event_loop.set_control_flow(ControlFlow::Wait);
+        }
+    }
+
+    let event_loop = EventLoop::new().unwrap();
+
+    event_loop.set_control_flow(ControlFlow::Wait);
+
+    event_loop.run_app(&mut App).unwrap();
 }
