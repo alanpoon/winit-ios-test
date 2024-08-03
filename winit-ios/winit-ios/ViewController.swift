9de86c7f269c90965ce624af9c7a702dd2a08a59
Alan Poon <brightshadowx@gmail.com>
2024-08-04 00:39:12 +0800

    webkit

---
+++ winit-ios/winit-ios/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  winit-ios
+//
+//  Created by user265135 on 8/3/24.
+//
+
+import SwiftUI
+
+struct ViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewController()
+}
